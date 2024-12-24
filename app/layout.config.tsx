559563a67eac2ba214c59bac56e0ea9905a7853d
Adithya Krishna <aadithya794@gmail.com>
2024-12-24 13:25:12 +0530

    chore: remove dropdown

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -11,11 +11,4 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}><img src="/assets/logo.png" alt="logo" width={25} height={25} />Reclaim Protocol Docs</div>,
   },
-  links: [
-    {
-      text: 'Documentation',
-      url: '/',
-      active: 'url',
-    },
-  ],
 };
